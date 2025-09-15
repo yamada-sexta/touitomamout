a2828c5c97d3a259ef2e6d759930d475328ef545
Yamada <114621472+yamada-sexta@users.noreply.github.com>
2025-09-15 16:34:52 -0500

    Remove docs

--- docs/docs/configuration/sections/_tip-file-generator.md
+++
@@ -1,4 +0,0 @@
-:::tip Environment file generator
-
-Feel free to generate your `.env` file with the [generator](/docs/tools/env-generator)
-:::
--- docs/src/components/environment-generator/utils/index.js
+++
@@ -1,4 +0,0 @@
-const getFormattedLabel = (field) =>
-  field.label ?? field.name.charAt(0).toUpperCase() + field.name.slice(1);
-
-export { getFormattedLabel };
