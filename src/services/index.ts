98183c809f13ab40e7b6c62754afcf5fba16f7b5
Yamada <114621472+yamada-sexta@users.noreply.github.com>
2025-09-15 21:07:42 -0500

    Clean up

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,2 @@
-export * from "./bluesky-sender";
-export * from "./mastodon-sender";
-// export * from "/helpers/download-media";
 export * from "./posts-synchronizer";
-export * from "./profile-synchronizer-old";
 export * from "./tweets-getter";
