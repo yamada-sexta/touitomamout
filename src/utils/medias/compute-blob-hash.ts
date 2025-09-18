712a2e2daafb5cade5285c4cc2f6406707f2ce6e
Yamada <114621472+yamada-sexta@users.noreply.github.com>
2025-09-18 13:43:58 -0500

    Use bun hash

--- src/utils/medias/compute-blob-hash.ts
+++ src/utils/medias/compute-blob-hash.ts
@@ -1,16 +1,12 @@
-import { subtle } from "node:crypto";
-
 import { DEBUG } from "env";
 
 export const computeBlobHash = async (blob: Blob): Promise<string> => {
+  const hasher = new Bun.CryptoHasher("sha256");
   const buffer = await blob.arrayBuffer();
-  const hashBuffer = await subtle.digest("SHA-256", buffer);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hash = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
-
+  hasher.update(buffer);
+  const hash = hasher.digest("hex");
   if (DEBUG) {
     console.log(`Computed hash: ${hash}`);
   }
-
   return hash;
 };
